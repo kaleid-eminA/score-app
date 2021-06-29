a31f93871ad254ff2c73d961da37976e2162d0f6
kaleid-eminA <kaleidliner.illyasviel@gmail.com>
2021-06-29 16:18:37 +0900

    文字修正

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,6 +2,6 @@ class RankingsController < ApplicationController
 
   def index
     @users = User.all
-    @scores = Score.all.includes(:user).order('score DESC').limit(10)
+    @scores = Score.all.includes(:user).order('score DESC').limit(30)
   end
 end
